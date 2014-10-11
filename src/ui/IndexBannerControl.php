db7863106a5f20da549dd32d2d47d6ff77b1c147
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-10-12 01:29:48 +1100

    Tweak messaging on index banner

--- src/ui/IndexBannerControl.php
+++ src/ui/IndexBannerControl.php
@@ -8,7 +8,7 @@ final class IndexBannerControl extends Control {
   <h1>Protobuild packages</h1>
   <p class="lead">
     Packages without the pain.  Cross-platform packages that
-    let you switch out binaries for source code.
+    let you switch between binaries and source code seamlessly.
   </p>
   <p><button type="button" class="btn btn-lg btn-primary" disabled="disabled" href="#">Coming soon</button></p>
 </div>
